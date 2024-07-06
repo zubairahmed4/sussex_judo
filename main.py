9a29262564da6d1a7d3483dd6bb2195928fdb0f5
zubairahmed4 <98022101+zubairahmed4@users.noreply.github.com>
2024-07-07 00:57:51 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+from start_page import StartPage
+
+if __name__ == "__main__":
+    StartPage()
---
+++ test.py
@@ -0,0 +1,7 @@
+training_plan = "Elite"
+
+if training_plan in ["Beginner", "B", "b"]:
+    print("Beginner")
+else:
+    print(f"Player is {training_plan}")
+
